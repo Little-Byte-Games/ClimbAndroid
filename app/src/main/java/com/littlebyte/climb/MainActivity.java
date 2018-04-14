8eb2d5a8acc2cb76585481906064c7fc33b03186
Steve Yeager <steve.yeager@littlebytegames.com>
2018-04-14 10:53:35 -0700

    Use register api
    Will be removed once web is working.

--- app/src/main/java/com/littlebyte/climb/MainActivity.java
+++ app/src/main/java/com/littlebyte/climb/MainActivity.java
@@ -3,56 +3,15 @@ package com.littlebyte.climb;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-
-import java.util.List;
-
-import io.swagger.client.api.SampleDataApi;
-import io.swagger.client.model.WeatherForecast;
-
-class DataResponse implements Listener<List<WeatherForecast>> {
-    final private TextView mTextView;
-
-    public DataResponse(TextView textView) {
-        mTextView = textView;
-    }
-
-    @Override
-    public void onResponse(List<WeatherForecast> response) {
-        String text = "";
-        if (response != null) {
-            for (int i = 0; i < response.size(); i++) {
-                text += response.get(i).toString() + "\n";
-            }
-        }
-        mTextView.setText(text);
-    }
-}
-
-class DataError implements Response.ErrorListener {
-    final private TextView mTextView;
-
-    public DataError(TextView textView) {
-        mTextView = textView;
-    }
-
-    @Override
-    public void onErrorResponse(VolleyError error) {
-        mTextView.setText(error.getMessage());
-    }
-}
-
+import io.swagger.client.api.AccountApi;
+import io.swagger.client.model.ApplicationUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -62,36 +21,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void loadData(View view) {
+    public void onRegisterClick(View view) {
         final TextView mTextView = findViewById(R.id.textView);
         mTextView.setText("loading...");
 
-        SampleDataApi sampleData = new SampleDataApi();
-        sampleData.setBasePath("http://192.168.196.1:45455");
+        AccountApi accountApi = new AccountApi();
+        accountApi.setBasePath("http://192.168.196.1:45455");
 
-        sampleData.sampleDataWeatherForecasts(new DataResponse(mTextView), new DataError(mTextView));
-    }
+        final EditText emailInput = findViewById(R.id.emailInput);
+        final String email = emailInput.getText().toString();
 
-    private void test() {
-        final TextView mTextView = findViewById(R.id.textView);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://192.168.196.1:45456/api/v1/SampleData/WeatherForecasts";
+        final EditText passwordInput = findViewById(R.id.passwordInput);
+        final String password = passwordInput.getText().toString();
 
-        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+        final EditText confirmInput = findViewById(R.id.confirmInput);
+        final String confirm = confirmInput.getText().toString();
+
+        accountApi.accountRegister(email, password, confirm, new Listener<ApplicationUser>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        mTextView.setText(response.toString());
+                    public void onResponse(ApplicationUser response) {
+                        mTextView.setText("Welcome " + response.getEmail());
                     }
                 }, new ErrorListener() {
-
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        mTextView.setText(error.getMessage());
+                        mTextView.setText("Nope");
 
                     }
-                });
-
-        queue.add(jsonObjectRequest);
+                }
+        );
     }
 }
